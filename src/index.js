a8fd03d63eb359475e0dcd9abf1c2edd5ce5ea6b
Nishan <nishan.bende@solutelabs.com>
2019-05-08 09:05:23 +0530

    Dev tools support

--- src/index.js
+++ src/index.js
@@ -1,61 +1,80 @@
-import React from "react";
 import { useReducer, useMemo, useEffect } from "react";
 
 let stores = {};
 let subscribers = {};
+let counter = 0;
 
-const withDevTools = (
-    process.env.NODE_ENV === "development" && typeof window !==  'undefined' 
-    && window.__REDUX_DEVTOOLS_EXTENSION__
-);
-
+const REDUX_DEVTOOL_SET_STATE = "REDUX_DEVTOOL_SET_STATE";
+const withDevTools =
+  process.env.NODE_ENV === "development" &&
+  typeof window !== "undefined" &&
+  window.__REDUX_DEVTOOLS_EXTENSION__;
 
 const devToolReducer = reducer => (state, action) => {
-    if(action && action.type === "DISPATCH") {
-        if(action.payload.type === "JUMP_TO_STATE" || action.payload.type==="JUMP_TO_ACTION"){
-            return action.state;
-        }
-    } 
-     else {
-        return reducer(state, action);
-    }
-}
-
+  if (action.type === REDUX_DEVTOOL_SET_STATE) {
+    return action.state;
+  } else {
+    return reducer(state, action);
+  }
+};
 
 function useReducerWithThunk(reducer, initialState, name) {
-    let memoizedValue = reducer;
-    if(withDevTools) {
-        memoizedValue = useMemo(() => devToolReducer(reducer), [reducer]);
-    }   
-    const [state, dispatch] = useReducer(memoizedValue, initialState);
+  let memoizedValue = reducer;
 
-    useEffect(()=>{
-        if(withDevTools){
-            stores[name] = window.__REDUX_DEVTOOLS_EXTENSION__.connect({name});
-            stores[name].init({ value: initialState });
-    
-            subscribers[name] = stores[name].subscribe((message) => {
-                dispatch(message);
-            });
-        }
-        return () => {
-            if(withDevTools){
-                subscribers[name]();
-            }
-        }
-    },[])
+  // Memoizing to prevent recreation of devtoolReducer on each render.
+  if (withDevTools) {
+    memoizedValue = useMemo(() => devToolReducer(reducer), [reducer]);
+  }
 
-    const customDispatch = (action) => {
-        if(typeof action === 'function'){
-            return action(customDispatch);
-        }else{
-            dispatch(action);
-            if(window && window.__REDUX_DEVTOOLS_EXTENSION__ && withDevTools) {
-                stores[name].send(action, reducer(state, action));
-            }
-        }
+  const [state, dispatch] = useReducer(memoizedValue, initialState);
+
+  useEffect(() => {
+    if (withDevTools) {
+      if (stores[name]) {
+        throw new Error("Name already exists ", name);
+      }
+      memoizedValue.prototype.name = name;
+      if (!memoizedValue.prototype.name) {
+        memoizedValue.prototype.name = document.title + "_" + counter;
+        counter++;
+      }
+
+      stores[
+        memoizedValue.prototype.name
+      ] = window.__REDUX_DEVTOOLS_EXTENSION__(reducer, initialState, {
+        name: memoizedValue.prototype.name
+      });
+
+      subscribers[memoizedValue.prototype.name] = stores[
+        memoizedValue.prototype.name
+      ].subscribe(() => {
+        dispatch({
+          type: REDUX_DEVTOOL_SET_STATE,
+          state: stores[memoizedValue.prototype.name].getState()
+        });
+      });
+    }
+
+    return () => {
+      if (withDevTools) {
+        subscribers[memoizedValue.prototype.name]();
+        stores[memoizedValue.prototype.name] = undefined;
+      }
     };
-    
-    return [state, customDispatch];
+  }, []);
+
+  const customDispatch = action => {
+    if (typeof action === "function") {
+      return action(customDispatch);
+    } else {
+      if (withDevTools && stores[memoizedValue.prototype.name]) {
+        stores[memoizedValue.prototype.name].dispatch(action);
+      } else {
+        dispatch(action);
+      }
+    }
+  };
+
+  return [state, customDispatch];
 }
 export default useReducerWithThunk;
