2f2a28e08df37157f9d9bc31c29a637cb228a34b
Nishan <nishan.bende@solutelabs.com>
2019-06-10 18:03:25 +0530

    Prevent devtools setup if no name is passed

--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ let subscribers = {};
 let counter = 0;
 
 const REDUX_DEVTOOL_SET_STATE = "REDUX_DEVTOOL_SET_STATE";
-const withDevTools =
-  process.env.NODE_ENV === "development" &&
-  typeof window !== "undefined" &&
-  window.__REDUX_DEVTOOLS_EXTENSION__;
+const withDevTools = name => {
+  return (
+    name &&
+    process.env.NODE_ENV === "development" &&
+    typeof window !== "undefined" &&
+    window.__REDUX_DEVTOOLS_EXTENSION__
+  );
+};
 
 const devToolReducer = reducer => (state, action) => {
   if (action.type === REDUX_DEVTOOL_SET_STATE) {
@@ -20,16 +24,17 @@ const devToolReducer = reducer => (state, action) => {
 
 function useReducerWithThunk(reducer, initialState, name) {
   let memoizedReducer = reducer;
+  let shouldConfigDevTools = withDevTools(name);
 
   // Memoizing to prevent recreation of devtoolReducer on each render.
-  if (withDevTools) {
+  if (shouldConfigDevTools) {
     memoizedReducer = useMemo(() => devToolReducer(reducer), [reducer]);
   }
 
   const [state, dispatch] = useReducer(memoizedReducer, initialState);
 
   useEffect(() => {
-    if (withDevTools) {
+    if (shouldConfigDevTools) {
       memoizedReducer.prototype.name = getReducerName(name);
 
       if (stores[memoizedReducer.prototype.name]) {
@@ -53,7 +58,7 @@ function useReducerWithThunk(reducer, initialState, name) {
     }
 
     return () => {
-      if (withDevTools) {
+      if (shouldConfigDevTools) {
         subscribers[memoizedReducer.prototype.name]();
         subscribers[memoizedReducer.prototype.name] = undefined;
         stores[memoizedReducer.prototype.name] = undefined;
@@ -65,7 +70,7 @@ function useReducerWithThunk(reducer, initialState, name) {
     if (typeof action === "function") {
       return action(customDispatch);
     } else {
-      if (withDevTools && stores[memoizedReducer.prototype.name]) {
+      if (shouldConfigDevTools && stores[memoizedReducer.prototype.name]) {
         stores[memoizedReducer.prototype.name].dispatch(action);
       } else {
         dispatch(action);
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
-var path = require('path');
+var path = require("path");
 module.exports = {
-  entry: './src/index.js',
-  mode: 'production',
+  entry: "./src/index.js",
+  mode: "production",
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'index.js',
-    libraryTarget: 'commonjs2'
+    path: path.resolve(__dirname, "build"),
+    filename: "index.js",
+    libraryTarget: "commonjs2"
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        include: path.resolve(__dirname, 'src'),
+        include: path.resolve(__dirname, "src"),
         exclude: /(node_modules|bower_components|build)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader"
         }
       }
     ]
   },
   externals: {
-    'react': 'commonjs react' 
+    react: "commonjs react"
   }
 };
