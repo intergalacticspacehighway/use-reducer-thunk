d60dd55a2b38e02fce719237b28a42328136ff98
Nishan <nishan@Nishans-MacBook-Pro.local>
2020-09-14 14:43:42 +0530

    added example

--- example/src/App.js
+++ example/src/App.js
@@ -1,22 +1,96 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { useReducerThunk } from "../../src/index";
+import useReducerWithThunk from "../../src/index";
 
-const reducer = (state, action) => {
-  return state;
-};
+function reducer(state, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case "DECREMENT":
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+    case "SUCCESS":
+      return {
+        ...state,
+        loading: false,
+      };
+    case "LOADING":
+      return {
+        ...state,
+        loading: true,
+      };
+    default:
+      return state;
+  }
+}
 
 function App() {
-  const [state, dispatch] = useReducerThunk(reducer, {}, "test");
-  console.log("state ", dispatch, state);
+  const [state, dispatch] = useReducerWithThunk(
+    reducer,
+    { count: 0, loading: false },
+    "example"
+  );
+
+  const { count, loading } = state;
+
+  function handleIncrement() {
+    dispatch({ type: "INCREMENT" });
+  }
+
+  function handleDecrement() {
+    dispatch({ type: "DECREMENT" });
+  }
+
+  function asyncIncrementAction() {
+    return (dispatch, getState) => {
+      dispatch({ type: "LOADING" });
+      console.log("prev state ", getState());
+
+      setTimeout(() => {
+        handleIncrement();
+        dispatch({ type: "SUCCESS" });
+      }, 1000);
+    };
+  }
+
+  function asyncDecrementAction() {
+    return (dispatch, getState) => {
+      dispatch({ type: "LOADING" });
+      console.log("prev state ", getState());
+
+      setTimeout(() => {
+        handleDecrement();
+        dispatch({ type: "SUCCESS" });
+      }, 1000);
+    };
+  }
+
+  function handleAsyncIncrement() {
+    dispatch(asyncIncrementAction());
+  }
+
+  function handleAsyncDecrement() {
+    dispatch(asyncDecrementAction());
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <input type="number"></input>
-        <button>Increment</button>
+        <input type="number" readOnly value={count}></input>
+        <button onClick={handleAsyncIncrement} disabled={loading}>
+          Increment after 1 second
+        </button>
+        <button onClick={handleAsyncDecrement} disabled={loading}>
+          {loading ? "loading..." : "Decrement after 1 second"}
+        </button>
+        <div>{loading && "loading..."}</div>
       </header>
     </div>
   );
